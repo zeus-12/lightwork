ed6aa53c8aab78ca78a2c7e34b958e326816261e
vishnu <vishnuvinod072@gmail.com>
2025-03-03 23:49:53 +0530

    setup express routing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("hello.");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import Router from "@/routes/index";
+import { corsOptionsDelegate, PORT } from "@/lib/config";
+
+dotenv.config();
+const app = express();
+
+app
+  .listen(PORT)
+  .on("listening", () => {
+    console.log(`Server is running on port ${PORT}`);
+  })
+  .on("error", (err: any) => {
+    console.log(err);
+  });
+
+app.use(cors(corsOptionsDelegate));
+app.use(express.json());
+app.use("/api", Router);
